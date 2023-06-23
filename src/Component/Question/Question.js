ef1583d0e5a1e5849cfd5a93e24837655c9ac0d8
unknown <tawhidaritu1@gmail.com>
2023-06-23 22:03:33 +0600

    Question-ans updated

--- src/Component/Question/Question.js
+++ src/Component/Question/Question.js
@@ -7,7 +7,7 @@ const Question = () => {
           <div className="question-cart">
             <h3>How React Works?</h3>
             <p>
-              React works by using a virtual representation of the browser's Document Object Model (DOM), called the Virtual DOM. It efficiently updates the UI by comparing the previous and current versions of the Virtual DOM and applying the necessary changes to the actual DOM. React follows a component-based approach, where the UI is broken down into reusable components. Each component manages its own state and can receive data through props. React components have a render method that describes what the UI should look like. React enforces a one-way data flow, where data is passed from parent components to child components through props.
+              React works by using a virtual representation of the browser's Document Object Model (DOM), called the Virtual DOM. It efficiently updates the UI by comparing the previous and current versions of the Virtual DOM and applying the necessary changes to the actual DOM.Each component manages its own state and can receive data through props. React components have a render method that describes what the UI should look like. React enforces a one-way data flow, where data is passed from parent components to child components through props.
             </p>
           </div>
           <div className="question-cart">
@@ -15,20 +15,17 @@ const Question = () => {
             <p>
              In React, both state and props are used to manage and pass data within components, but they have some fundamental differences:
              State:
-             1. State is a built-in feature of React components and is managed internally by the component itself.
-             2. It represents the mutable data that can be changed over time and affects the component's behavior and rendering.
-             3. State changes trigger the re-rendering of the component and its child components, allowing the UI to reflect the updated state.
+             State is a built-in feature of React components and is managed internally by the component itself.It represents the mutable data that can be changed over time and affects the component's behavior and rendering.
              Props:
-             1. Props (short for properties) are inputs passed to a component from its parent component.
-             2. Props are read-only and cannot be modified by the component receiving them.
-             3. They represent the data that a parent component wants to pass down to its child components.
-             4. Props are defined when a component is invoked, and their values are determined by the parent component.
+             Props (properties) are inputs passed to a component from its parent component.Props are read-only and cannot be modified by the component receiving them.
+            They represent the data that a parent component wants to pass down to its child components.
             </p>
           </div>
           <div className="question-cart">
             <h3>How useState Work?</h3>
             <p>
-              
+            useState is a React hook that allows functional components to manage state. Here's a short explanation of how it works:
+             Inside the functional component, call useState and provide an initial value for the state variable. It returns an array with two elements: the current state value and a function to update the state.Accessing State: You can access the current state value (state) in your component's JSX or within its functions.It provides a simple way to declare and manage state in functional components, enabling you to create dynamic and interactive UIs.
             </p>
           </div>
         </div>
